eb4ae21d791fedadcb79a78559b30be5c56e3daa
ZhiQiang.Fan <571253675@qq.com>
2026-03-08 20:44:44 +0800

    Remove  Extra Files

--- run.sh
+++ run.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-# 完整部署（较慢）：清理 → 生成 → 部署
-# 仅本地预览（更快）：hexo g && hexo s
 flag=$1
 if [ "$flag" == clean ]; then
   hexo clean
 elif [ "$flag" == d ]; then
   hexo d
 else
-  hexo clean
-  hexo g
-  hexo s
+  hexo clean &&  hexo g &&   hexo s
 fi
